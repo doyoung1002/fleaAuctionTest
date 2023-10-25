e00f97241db630a34ec79063fe59df3fb53e156f
doyoung1002 <leedoyoung.oc@gmail.com>
2023-10-25 21:11:27 +0900

    update : sse

--- component/App.js
+++ component/App.js
@@ -1,33 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions, RefreshControl } from 'react-native';
 import EventSource from 'react-native-sse';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
 
-  const es = new EventSource('https://api.fleaauction.world/v2/sse/event');
+  const [auctions, setAuctions] = useState([]); // 상태 값 설정
+  const [refreshing, setRefreshing] = useState(false);
 
-  es.addEventListener('open', (event) => {
-    console.log('Open SSE connection');
-  });
+  const loadData = () => {
+    const es = new EventSource('https://api.fleaauction.world/v2/sse/event');
 
-  es.addEventListener("message", (event) => {
-    console.log("New message event:", event.data);
-  });
+    let loadedAuctions = [];
 
-  es.addEventListener("error", (event) => {
-    if (event.type === "error") {
-      console.error("Connection error:", event.message);
-    } else if (event.type === "exception") {
-      console.error("Error:", event.message, event.error);
-    }
-  });
+    es.addEventListener('sse.auction_viewed', (event) => {
+      console.log(event.data);
+      const eventData = JSON.parse(event.data);
+      const auctionData = {
+        id: event.lastEventId,
+        ...eventData
+      };
 
-  es.addEventListener("close", (event) => {
-    console.log("Close SSE connection.");
-  });
+      loadedAuctions.push(auctionData);
 
+      if (loadedAuctions.length >= 20) {
+        setAuctions(prevAuctions => [...prevAuctions, ...loadedAuctions]);
+        es.close();
+      }
+    });
+  };
+
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setAuctions([]);
+    loadData();
+    setRefreshing(false);
+  };
 
   return (
     <View style={styles.container}>
@@ -35,42 +49,31 @@ export default function App() {
         <Text>헤더 부분</Text>
       </View>
 
-      <ScrollView pagingEnabled horizontal contentContainerStyle style={styles.pictureList}>
-        <View style={styles.picture}>
-          <Text style={styles.auctionId}>10</Text>
-          <Text style={styles.viewCount}>1</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionId}>20</Text>
-          <Text style={styles.viewCount}>2</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionId}>30</Text>
-          <Text style={styles.viewCount}>3</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionId}>40</Text>
-          <Text style={styles.viewCount}>4</Text>
-        </View>
+      <ScrollView pagingEnabled horizontal style={styles.pictureList}>
+        {auctions.map((auction, index) => (
+          <View key={index} style={styles.picture}>
+            <Text style={styles.auctionNumber}>작품ID({auction.auctionId})</Text>
+            <Text style={styles.viewNumber}>조회수: {auction.viewCount}</Text>
+          </View>
+        ))}
       </ScrollView>
-
 
       <ScrollView pagingEnabled horizontal contentContainerStyle style={styles.paintingSecond}>
         <View style={styles.picture}>
-          <Text style={styles.auctionId}>100</Text>
-          <Text style={styles.viewCount}>5</Text>
+          <Text style={styles.auctionNumber}>100</Text>
+          <Text style={styles.viewNumber}>5</Text>
         </View>
         <View style={styles.picture}>
-          <Text style={styles.auctionId}>200</Text>
-          <Text style={styles.viewCount}>6</Text>
+          <Text style={styles.auctionNumber}>200</Text>
+          <Text style={styles.viewNumber}>6</Text>
         </View>
         <View style={styles.picture}>
-          <Text style={styles.auctionId}>300</Text>
-          <Text style={styles.viewCount}>7</Text>
+          <Text style={styles.auctionNumber}>300</Text>
+          <Text style={styles.viewNumber}>7</Text>
         </View>
         <View style={styles.picture}>
-          <Text style={styles.auctionId}>400</Text>
-          <Text style={styles.viewCount}>8</Text>
+          <Text style={styles.auctionNumber}>400</Text>
+          <Text style={styles.viewNumber}>8</Text>
         </View>
       </ScrollView>
 
@@ -87,7 +90,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
 
   },
   header: {
@@ -108,12 +110,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  auctionId: {
+  auctionNumber: {
     fontSize: 50,
     alignItems: 'center',
   },
 
-  viewCount: {
+  viewNumber: {
     fontSize: 30,
     alignItems: 'center',
   },
