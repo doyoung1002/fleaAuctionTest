95d36df60d8a7c4715480959bc7615ae396b1465
doyoung1002 <leedoyoung.oc@gmail.com>
2023-10-25 23:39:12 +0900

    update : random add

--- component/App.js
+++ component/App.js
@@ -9,38 +9,66 @@ export default function App() {
   const [auctions, setAuctions] = useState([]); // 상태 값 설정
   const [refreshing, setRefreshing] = useState(false);
 
+  const halfLength = Math.ceil(auctions.length / 2);
+  const topAuctions = auctions.slice(0, halfLength);
+  const bottomAuctions = auctions.slice(halfLength);
+
+  const isAuctionIdInBoth = (auctionId) => {
+    return topAuctions.some(a => a.auctionId === auctionId) && bottomAuctions.some(a => a.auctionId === auctionId);
+  };
+
+
   const loadData = () => {
     const es = new EventSource('https://api.fleaauction.world/v2/sse/event');
 
-    let loadedAuctions = [];
-
     es.addEventListener('sse.auction_viewed', (event) => {
-      console.log(event.data);
       const eventData = JSON.parse(event.data);
       const auctionData = {
         id: event.lastEventId,
         ...eventData
       };
 
-      loadedAuctions.push(auctionData);
-
-      if (loadedAuctions.length >= 20) {
-        setAuctions(prevAuctions => [...prevAuctions, ...loadedAuctions]);
-        es.close();
-      }
+      setAuctions(prevAuctions => {
+        if (prevAuctions.length < 20) {
+          return [...prevAuctions, auctionData];
+        } else {
+          es.close();
+          return prevAuctions;
+        }
+      });
     });
+
+    return () => {
+      es.close();
+    };
   };
 
-
   useEffect(() => {
-    loadData();
+    const cleanup = loadData(); // loadData 함수에서 반환된 cleanup 함수를 저장합니다.
+
+    return () => {
+      cleanup(); // 컴포넌트가 언마운트되거나 useEffect가 다시 실행될 때 cleanup 함수를 호출합니다.
+    };
   }, []);
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
 
   const onRefresh = () => {
     setRefreshing(true);
     setAuctions([]);
-    loadData();
+    const cleanup = loadData(); // loadData 함수에서 반환된 cleanup 함수를 저장합니다.
     setRefreshing(false);
+
+    return () => {
+      cleanup(); // loadData 함수의 이벤트 리스너를 닫습니다.
+    };
   };
 
   return (
@@ -49,32 +77,36 @@ export default function App() {
         <Text>헤더 부분</Text>
       </View>
 
-      <ScrollView pagingEnabled horizontal style={styles.pictureList}>
-        {auctions.map((auction, index) => (
+      <ScrollView pagingEnabled horizontal style={styles.pictureList} refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }>
+        {topAuctions.map((auction, index) => (
           <View key={index} style={styles.picture}>
             <Text style={styles.auctionNumber}>작품ID({auction.auctionId})</Text>
-            <Text style={styles.viewNumber}>조회수: {auction.viewCount}</Text>
+            {isAuctionIdInBoth(auction.auctionId) && (
+              <Text style={styles.viewNumber}>조회수: {auction.viewCount}</Text>
+            )}
           </View>
         ))}
       </ScrollView>
 
-      <ScrollView pagingEnabled horizontal contentContainerStyle style={styles.paintingSecond}>
-        <View style={styles.picture}>
-          <Text style={styles.auctionNumber}>100</Text>
-          <Text style={styles.viewNumber}>5</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionNumber}>200</Text>
-          <Text style={styles.viewNumber}>6</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionNumber}>300</Text>
-          <Text style={styles.viewNumber}>7</Text>
-        </View>
-        <View style={styles.picture}>
-          <Text style={styles.auctionNumber}>400</Text>
-          <Text style={styles.viewNumber}>8</Text>
-        </View>
+      <ScrollView pagingEnabled horizontal style={styles.paintingSecond} refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }>
+        {bottomAuctions.map((auction, index) => (
+          <View key={index} style={styles.picture}>
+            <Text style={styles.auctionNumber}>작품ID({auction.auctionId})</Text>
+            {isAuctionIdInBoth(auction.auctionId) && (
+              <Text style={styles.viewNumber}>조회수: {auction.viewCount}</Text>
+            )}
+          </View>
+        ))}
       </ScrollView>
 
       <View style={styles.footer}>
